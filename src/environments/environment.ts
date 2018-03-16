447ce1a28642552bea18ab353693718fc8c4c772
iqbal125 <iqbal125@yahoo.com>
2018-03-16 12:20:28 -0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-          apiKey: "AIzaSyC3l2wAO7HkJ9biEXU-NnUhQeMsSHvvwz8",
-          authDomain: "angwebsite-c2ab0.firebaseapp.com",
-          databaseURL: "https://angwebsite-c2ab0.firebaseio.com",
-          projectId: "angwebsite-c2ab0",
-          storageBucket: "gs://angwebsite-c2ab0.appspot.com",
-          messagingSenderId: "534164888530"}
+          apiKey: 
+          authDomain: 
+          databaseURL: 
+          projectId: 
+          storageBucket: 
+          messagingSenderId: 
+  }
 };
